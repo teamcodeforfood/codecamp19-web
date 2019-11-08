c103e9852a13be8772c1da80b598ff990f8bb4b0
Shane Taylor <shanetaylor@shanetaylor.local>
2019-11-08 09:28:21 -0700

    router setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
 import "./App.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
-  return <div>asdf</div>;
+  return (
+    <div>
+      <Switch>
+        <Route path="/">
+          Hello there
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Provider } from "react-redux";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import { store } from "./store";
 import "./index.css";
@@ -9,7 +10,9 @@ import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById("root")
 );
