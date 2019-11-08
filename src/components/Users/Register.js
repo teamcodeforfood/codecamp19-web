b64b57685e8f06a0fa85b27aadd318de072cebc8
Joshua Beitler <joshbeitler@gmail.com>
2019-11-08 12:44:39 -0700

    Register

--- src/components/Users/Register.js
+++ src/components/Users/Register.js
@@ -1,19 +1,43 @@
 import React from "react";
 import { Button, Card, Input, InputGroup, Intent } from "amino-ui";
+import useSWR from "swr";
 
 import { LoginWrapper } from "./LoginWrapper";
 import { Logo } from "../Layout/Logo";
 import { useInput } from "react-hanger";
 
 export const Register = () => {
-  const register = e => {
+  const register = async e => {
     e.preventDefault();
     e.stopPropagation();
 
-    if (password !== passwordVerify) {
+    if (password.value !== passwordVerify.value) {
       // TODO: prettier error message
       alert("Passwords don't match");
       return;
+    }
+
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_API_URL}/users/register`,
+        {
+          method: "POST",
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            email: email.value,
+            password: password.value
+          })
+        }
+      );
+
+      const result = await response.json();
+
+      console.log(result);
+    } catch (e) {
+      alert("error");
     }
   };
 
@@ -31,7 +55,12 @@ export const Register = () => {
             <Input label="Email address" type="email" required {...email} />
             <Input label="Full name" required {...name} />
             <Input type="password" label="Password" required {...password} />
-            <Input type="password" label="Password (again)" required {...passwordVerify} />
+            <Input
+              type="password"
+              label="Password (again)"
+              required
+              {...passwordVerify}
+            />
           </InputGroup>
           <Button intent={Intent.Primary}>Register</Button>
         </form>
